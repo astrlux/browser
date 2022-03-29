28d3705efd0cf678350fb571b30af35ed83c1567
astrlux <37908451+astrlux@users.noreply.github.com>
2022-03-28 20:11:30 -0700

    update user-overrides.js to incorporate letterboxing fix for browser (makes more vulnerable to finger-printing though...

--- firefox/user-overrides.js
+++ firefox/user-overrides.js
@@ -1,2 +1,4 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("svg.context-properties.content.enabled", true);
+user_pref("privacy.window.maxInnerWidth", 1980);
+user_pref("privacy.window.maxInnerHeight", 1020);
+user_pref("privacy.resistFingerprinting.letterboxing", false);
